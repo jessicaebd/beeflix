3271659de693f5c04a08ee44826e96e85984caf1
jessicaebd <jessica.natalia23oct@gmail.com>
2022-05-27 12:23:30 +0700

    init

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">BeeFlix</a>
+    </div>
+    <div class="container-fluid d-flex justify-content-start">
+        <button type="button" class="btn btn-dark">Kembali</button>
+        <button type="button" class="btn btn-outline-dark">Lihat Semua Film</button>
+    </div>
+</nav>
